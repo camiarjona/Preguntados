1c2433b138419334c5595d7b8d9dfffcc5c9b788
camiarjona <camarjona01@gmail.com>
2024-11-16 15:59:21 -0300

    Creacion de clase

---
+++ src/Gestion/GestionPreguntas.java
@@ -0,0 +1,13 @@
+package Gestion;
+
+import Modelo.Juego.Pregunta;
+
+public class GestionPreguntas {
+    private GestionDeElementos<Pregunta> preguntas;
+
+    public GestionPreguntas() {
+        preguntas = new GestionDeElementos<>();
+    }
+
+
+}
