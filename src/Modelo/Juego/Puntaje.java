f644e4c132ba696021b956d4f67e2abf6b478218
camiarjona <camarjona01@gmail.com>
2024-11-17 18:40:39 -0300

    Metodos json

--- src/Modelo/Juego/Puntaje.java
+++ src/Modelo/Juego/Puntaje.java
@@ -1,19 +1,31 @@
 package Modelo.Juego;
 
 import Interfaces.IJson;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Puntaje implements IJson<Puntaje> {
+import java.util.jar.JarException;
+
+public class Puntaje implements IJson {
 
     private int puntaje;
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
 
     @Override
     public JSONObject toJson() {
         return null;
     }
 
-    @Override
-    public Puntaje toObj() {
-        return null;
-    }
+   public static Puntaje fromJson(JSONObject j) {
+        Puntaje puntaje = new Puntaje();
+        try {
+            puntaje.setPuntaje(j.getInt("Puntaje"));
+        }catch (JSONException e) {
+           e.printStackTrace();
+        }
+       return puntaje;
+   }
 }
