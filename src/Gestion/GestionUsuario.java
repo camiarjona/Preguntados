b9bc2d16362c2031a915e53db84f653334c4198f
camiarjona <camarjona01@gmail.com>
2024-11-18 15:55:24 -0300

    Json usuarios

--- src/Gestion/GestionUsuario.java
+++ src/Gestion/GestionUsuario.java
@@ -6,30 +6,32 @@ import Excepciones.Usuario.UsuarioExistente;
 import Excepciones.Usuario.UsuarioIncorrecto;
 import Modelo.Usuario.Jugador;
 import Modelo.Usuario.Usuario;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GestionUsuario {
 
-    private GestionDeElementos<Usuario> usuarios;
+    private GestionDeElementos<Jugador> usuarios;
 
     public GestionUsuario() {
         usuarios = new GestionDeElementos<>();
     }
 
-    public void agregarUsuario(Usuario usuario) {
+    public void agregarUsuario(Jugador usuario) {
         usuarios.agregarElemento(usuario);
     }
 
-    public void eliminarUsuario(Usuario usuario) {
+    public void eliminarUsuario(Jugador usuario) {
         usuarios.eliminarElemento(usuario);
     }
 
-    public boolean buscarUsuario(Usuario usuario) {
+    public boolean buscarUsuario(Jugador usuario) {
         return usuarios.buscarElemento(usuario);
     }
 
     // Método login
     public boolean verificarLogin(String nombreUsuario, String contrasenia) throws UsuarioIncorrecto, ContraseniaInconrrecta {
-        for (Usuario usuario : usuarios.getElementos()) {
+        for (Jugador usuario : usuarios.getElementos()) {
             if (usuario.getNombreUsuario().equalsIgnoreCase(nombreUsuario)) {
                 // Si el nombre de usuario es correcto, verifico la contraseña
                 if (usuario.getContrasenia().equals(contrasenia)) {
@@ -44,7 +46,7 @@ public class GestionUsuario {
     }
 
     public void existeUsuario(String nombreUsuario) throws UsuarioExistente {
-        for (Usuario usuario : usuarios.getElementos()) {
+        for (Jugador usuario : usuarios.getElementos()) {
             if (usuario.getNombreUsuario().equalsIgnoreCase(nombreUsuario)) {
                 throw new UsuarioExistente("\n\u001B[31mNombre de usuario existente.\u001B[0m");
             }
@@ -52,17 +54,16 @@ public class GestionUsuario {
     }
 
     public void existeMail(String mail) throws CorreoExistente {
-        for (Usuario usuario : usuarios.getElementos()) {
+        for (Jugador usuario : usuarios.getElementos()) {
             if (usuario.getEmail().equalsIgnoreCase(mail)) {
                 throw new CorreoExistente("\n\u001B[31mEl correo ya se encuentra registrado.\u001B[0m");
             }
         }
     }
 
-
     //metodo para obtener el jugador una vez inciada la sesion
     public Jugador obtenerJugadorPorNombre(String nombreUsuario) throws UsuarioIncorrecto {
-        for (Usuario usuario : usuarios.getElementos()) {
+        for (Jugador usuario : usuarios.getElementos()) {
             if (usuario instanceof Jugador && usuario.getNombreUsuario().equals(nombreUsuario)) {
                 return (Jugador) usuario;
             }
@@ -70,9 +71,14 @@ public class GestionUsuario {
         throw new UsuarioIncorrecto("El usuario no existe.");
     }
 
-
-
-
+    //metodo para convertir mi lista de usuarios a formato json
+    public JSONArray usuariosAJson(){
+        JSONArray jsonArray = new JSONArray();
+        for (Jugador usuario : usuarios.getElementos()) {
+            jsonArray.put(usuario.toJson());
+        }
+        return jsonArray;
+    }
 
 
 }
