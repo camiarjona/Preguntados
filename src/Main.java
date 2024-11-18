86867767f640c50145005abbf39b11b925b9d54b
BelenIFigueroa <belenirmafigueroa@gmail.com>
2024-11-18 18:48:08 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import Gestion.GestionPreguntas;
+import Modelo.Enum.Categoria;
+import Modelo.Juego.Pregunta;
+import Modelo.Juego.PreguntaMultipleChoice;
+import Modelo.Juego.PreguntaVerdaderoOFalso;
+import Modelo.Juego.Ronda;
+import Modelo.Usuario.Jugador;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
